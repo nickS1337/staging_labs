27f64f819a37eade9045c5e0da2d018dd9b4debd
nickS1337 <nick.petjaten@gmail.com>
2023-01-04 23:53:28 +1100

    added PHPFPM-alpine. but can't seem to use it just yet.

--- multicontainer-server_start.sh
+++ multicontainer-server_start.sh
@@ -1,5 +1,6 @@
 
 #start the servers as daemons
+echo "MySQL Server starting up. Waiting 20 seconds before initial connection"
 docker run -p 3306:3306 \
     --name mysql-server \
     -e MYSQL_ROOT_PASSWORD=mygene \
@@ -8,13 +9,21 @@ docker run -p 3306:3306 \
     -v /home/nicholas/Desktop/mygene/intro_final/mysql:/docker-entrypoint-initdb.d \
     -d mysql:8
 
+echo "PHP server started"
+docker run -p 9000 \
+    --name php-server \
+    -d php:8.0-fpm-alpine
+
+echo "Nginx server started"
 docker run -p 8080:80 \
     -v /home/nicholas/Desktop/mygene/intro_final/public:/var/www/html/ \
     -v /home/nicholas/Desktop/mygene/intro_final/public:/usr/share/nginx/html \
     --name nginx-server \
     -d nginx:1.22-alpine
 
-echo "mysql server starting up. please wait 20 seconds...."
+#sleep 20
+#echo "Starting Node.JS server"
+#node server.js
 
 #copy data.sql so we can mount it:
 #docker cp ./data.sql mysql-server:/docker-entrypoint-inidtdb.d
@@ -24,7 +33,8 @@ echo "mysql server starting up. please wait 20 seconds...."
 docker cp public/. nginx-server:/usr/share/nginx/html/
 
 #add the nginx configuration file to the server
-#docker cp nginx.conf nginx-server:/etc/nginx/nginx.conf
+docker cp nginx.conf nginx-server:/etc/nginx/nginx.conf
+echo "Added Nginx configuration file to /etc/nginx/"
 
 #delete the apache default web page:
 #docker exec -it nginx-server rm /var/www/html/index.html
--- multicontainer-server_stop.sh
+++ multicontainer-server_stop.sh
@@ -1,5 +1,7 @@
 docker stop nginx-server
 docker stop mysql-server
+docker stop php-server
 
 docker rm nginx-server
 docker rm mysql-server
+docker rm php-server
